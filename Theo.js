4ebefaa6e8dff57b5c201393c27f887da072c353
Ekeobong Akpan <theostardaniel@gmail.com>
2025-04-11 05:38:59 +0100

    Add files via upload
    This files describe Data types used in Javascript.

---
+++ Theo.js
@@ -0,0 +1,27 @@
+// CLASS WORK ON JAVASCRIPT 
+
+//console.log("Welcome to Java Script Training")
+
+//DATA TYPES IN JS
+
+//VARIABLES
+var i_no_fit_to_understand = "Dada"
+var i_have_a_Confession_Chai = "obession"
+var listen_to_my_confession_mbok = "Omah lay"
+var loving_you_na_my_ritual = "My Woman"
+var da_girl_get_no_problem = "Annie Maculay"
+
+// numbers
+var she_loves_me_like_damn = 100
+var how_long_we_live_to_realise = 0.5
+var what_is_the_reason_see = 3
+var are_aware_of_the_things = 76
+var listen_to_your_self_come = 3.43
+
+//Boolean
+var baby_come_closer_no_tey = true
+var soso_take_breathe_away_yeah = false
+var i_can_stand_for_once = true
+var we_keep_it_100_no_pretence = false
+var one_more_shot_of_Tequila = true
+
